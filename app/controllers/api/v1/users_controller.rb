92571d677514bfb69904be682304a0911237eecb
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-22 16:01:17 +0530

    Login and Logout backend added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,30 @@
 class Api::V1::UsersController < ApplicationController
-  def index
+
+  def create
+    user= User.create(user_params);
+    if user.valid?
+      payload = {user_id: user.id}
+      token = encode_token(payload)
+      render json: { user: user, jwt: token}
+    else
+      render json: {errors: user.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  def login
+    user= User.find_by(email: params[:email])
+    if user && user.password== params[:password]
+      payload= {user_id: user.id}
+      token =encode_token(payload)
+      render json: {user: user,jwt: token, success: "Welocome back, #{user.name}"}
+    else
+      render json: { failure: "Log in Failed! invalid email or password"}
+    end 
+  end
+
+
+  private
+  def user_params
+    params.permit(:name,:username,:email,:password)
   end
 end

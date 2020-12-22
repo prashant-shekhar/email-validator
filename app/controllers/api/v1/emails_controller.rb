cd0411ae0c9c1d83e5c6d5e66b18119c705f408e
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-22 09:21:05 +0530

    database saved

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::EmailsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     @user=User.find(params[:userid])
     @emails= @user.emails
@@ -6,19 +7,13 @@ class Api::V1::EmailsController < ApplicationController
   end
 
   def create
-    @email = 'nikhi@gmail.com';
-    @res = Truemail.validate(@email)
+    @res = Truemail.validate(params[:email])
     if @res.result.success
-      @email= Email.new(email_params)
+      @email= Email.new
+      @email.email=params[:email]
+      @email.user_id=params[:id]
       @email.save
     end
     render json: @res.result.success
   end
-  # single email check -> if valid -> database store -> and return 
-
-
-  private
-    def email_params
-      params.require(:emails).permit(:email)
-    end
 end

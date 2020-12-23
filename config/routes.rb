e644baab0847b3d5faf406833eec16e617cf5871
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-23 23:02:23 +0530

    routes issue fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'emails/index'
-      resources :emails, only: [:create]
+      resources :emails, only: [:create,:index]
     end
   end
   namespace :api do

f59a9e561aec668d9ae6bcecc6c2be796d56063f
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-12-24 14:50:02 +0530

    Email test added

--- test/models/email_test.rb
+++ test/models/email_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EmailTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save email without email" do
+    email = Email.new
+    assert_not email.save, "Saved the email without email"
+  end
 end

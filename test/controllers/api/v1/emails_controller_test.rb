e12c913c691e3e99a114afdba8397b0d3a332a09
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-24 23:09:31 +0530

    response test completed

--- test/controllers/api/v1/emails_controller_test.rb
+++ test/controllers/api/v1/emails_controller_test.rb
@@ -15,5 +15,15 @@ class Api::V1::EmailsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "get response" do
+    get '/api/v1/emails/index'
+    assert_equal 200, response.status
+  end
+
+  test "create response" do
+    post '/api/v1/emails', params:{email: @email.email, userid: @email.user}
+    assert_equal 200, response.status
+  end
+
 
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -15,4 +15,14 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
     assert :success
   end
 
+  test "response create user" do
+    post '/api/v1/users', params:{name: @user.name, username: @user.username, email: @user.email, password: @user.password}, as: :json
+    assert_equal 200, response.status
+  end 
+
+  test "response login user" do
+    post '/api/v1/users/login', params:{email: 'email', password:'password'}, as: :json
+    assert_equal 200, response.status
+  end
+
 end

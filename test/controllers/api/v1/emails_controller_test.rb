49687f72710c1a0fc4a353c261ea07f07f8a6a20
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-24 22:51:20 +0530

    basic controller test completed

--- test/controllers/api/v1/emails_controller_test.rb
+++ test/controllers/api/v1/emails_controller_test.rb
@@ -1,4 +1,19 @@
 require 'test_helper'
 
 class Api::V1::EmailsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @email= emails(:valid)
+  end
+
+  test "should get index" do
+    get '/api/v1/emails/index'
+    assert_response :success
+  end
+
+  test "should create email" do
+    post '/api/v1/emails', params:{email: @email.email, userid: @email.user}
+    assert_response :success
+  end
+
+
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user= users(:valid)
+  end
+
+  test "should create user" do
+    post '/api/v1/users', params:{name: @user.name, username: @user.username, email: @user.email, password: @user.password}, as: :json
+    assert :success
+  end 
+
+  test "should login user" do
+    post '/api/v1/users/login', params:{email: 'email', password:'password'}, as: :json
+    assert :success
+  end
+
 end

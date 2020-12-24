c4b28d4d3870b76637565649e7d1d9a9dfb37aee
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-24 10:55:10 +0530

    frontend route fixed and backend varible changed

--- app/controllers/api/v1/emails_controller.rb
+++ app/controllers/api/v1/emails_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::EmailsController < ApplicationController
   def index
-    @user=User.find(params[:userid])
-    @emails= @user.emails
-    render json: @emails
+    user=User.find(params[:userid])
+    emails= user.emails
+    render json: emails
   end
 
   def create
-    @res = Truemail.validate(params[:email],with: :smtp)
-    if @res.result.success
-      @email= Email.new(email: params[:email],user_id: params[:userid] )
-      @email.save
+    res = Truemail.validate(params[:email],with: :smtp)
+    if res.result.success
+      email= Email.new(email: params[:email],user_id: params[:userid] )
+      email.save
     end
-    render json: @res.result.success
+    render json: res.result.success
   end
 end

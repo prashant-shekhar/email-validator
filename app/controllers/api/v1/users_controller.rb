e1324ae634718d286504129e66eba16f2ad9fa7e
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-12-25 20:32:01 +0530

    Back-End Functionality added for activating users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-
+  skip_before_action :verify_authenticity_token
   def index
     user = User.find(params[:user_id])
     puts params[:user_id]
@@ -17,7 +17,7 @@ class Api::V1::UsersController < ApplicationController
       if user.valid?
         payload = {user_id: user.id}
         token = encode_token(payload)
-        copy_user= user.slice(:id, :name, :email, :username)
+        copy_user= user.slice(:id, :name, :email, :username, :has_role)
         render json: { user: copy_user, jwt: token}
       else
         render json: {errors: user.errors.full_messages}, status: :not_acceptable
@@ -32,15 +32,31 @@ class Api::V1::UsersController < ApplicationController
     if user && User.authenticate(user.password,params[:password])
       payload= {user_id: user.id}
       token =encode_token(payload)
-      copy_user= user.slice("id","name","email","username")
+      copy_user= user.slice(:id , :name, :email, :username, :has_role)
       render json: {user: copy_user,jwt: token, error: false, message: "Welocome back, #{user.name}"}
     else
       render json: { error: true, message: "Log in Failed! invalid email or password"}, status: :not_acceptable
     end 
   end
 
+  def update
+    admin = User.find(params[:admin_id])
+    if admin.has_role == 'admin'
+      user = User.find(params[:id])
+      if user.update(user_params)
+        copy_user = user.slice(:id, :name, :email, :username, :has_role, :is_activated)
+        render json: {user: copy_user, message: "user activated successfully"}
+      else
+        render json: {errors: user.errors.full_messages}, status: :not_acceptable
+      end
+    else
+      render json: {error: true, message: "You are not authorize person."}, status: :unauthorized
+    end
+
+  end
+
   private
   def user_params
-    params.require(:user).permit(:name,:username,:email,:password, :has_role, :is_activated)
+    params.permit(:name,:username,:email,:password, :has_role, :is_activated)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :emails, only: [:create,:index]
+      resources :emails, only: [:create, :index, :update]
       post "users/login", to: "users#login"
-      resources :users, only: [:create,:index]
+      resources :users, only: [:create, :index, :update]
     end
   end
   root 'home#index'

bc86808705bc3b8145f88445a34b9f73223f937b
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-25 11:58:25 +0530

    test fixed

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -16,7 +16,7 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "valid response create user" do
-    post '/api/v1/users', params: {email: 'someone2@gmail.com', password:'password',name:'name1',username:'username1'}, as: :json
+    post '/api/v1/users', params: {email: 'nikhilbhatt@gmail.com', password:'password',name:'name1',username:'username2'}, as: :json
     assert_equal 200, response.status
   end 
 

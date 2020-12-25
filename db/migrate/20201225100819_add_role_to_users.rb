71840f6545a41228d007b00c8a205f8f039409ca
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-12-25 19:22:19 +0530

    default value added for has_role column in users table

--- db/migrate/20201225100819_add_role_to_users.rb
+++ db/migrate/20201225100819_add_role_to_users.rb
@@ -1,5 +1,5 @@
 class AddRoleToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :has_role, :string, null: false
+    add_column :users, :has_role, :string, null: false, default: "user"
   end
 end

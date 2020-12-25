23af67ff25a4d5b21d88189bde5d3d62c1a7531a
nikhilbhatt <nikhilbhatt931@gmail.com>
2020-12-25 10:45:20 +0530

    json content restricted

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,9 +6,10 @@ class Api::V1::UsersController < ApplicationController
       if user.valid?
         payload = {user_id: user.id}
         token = encode_token(payload)
-        render json: { user: user, jwt: token}
+        usr= user.slice("id","name","email","username")
+        render json: { user: usr, jwt: token}
       else
-        render json: {errors: user.errors.full_messages}, status: :not_acceptable
+        render json: {errors: user.errors.full_messages} , status: :not_acceptable
       end
     else
       render json: {errors: ['Invalid Email!']}, status: :not_acceptable
@@ -20,7 +21,8 @@ class Api::V1::UsersController < ApplicationController
     if user && User.authenticate(user.password,params[:password])
       payload= {user_id: user.id}
       token =encode_token(payload)
-      render json: {user: user,jwt: token, error: false, message: "Welocome back, #{user.name}"}
+      usr= user.slice("id","name","email","username")
+      render json: {user: usr,jwt: token, error: false, message: "Welocome back, #{user.name}"}
     else
       render json: { error: true, message: "Log in Failed! invalid email or password"}
     end 

e526114cff27f6e58aa4c41fecd7af691197a24a
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-12-27 17:46:46 +0530

    test for updating user done

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -11,13 +11,11 @@ class UserFlowTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  #Test for creating user depends on TRUEMAIL
-
   test 'should not create user' do
     assert_no_changes('User.count') do
-      post "/api/v1/users", params: {name: 'New User', email: 'prashant.shekhar@elitmus.com' ,username: 'New_Username', password: 'sample_password', has_role: 'user', is_activated: true}, as: :json
+      post "/api/v1/users", params: {name: 'New User', email: '' ,username: 'New_Username', password: 'sample_password', has_role: 'user', is_activated: true}, as: :json
     end
-    assert_response :success
+    assert_equal 406, response.status
   end
 
   test "login an user" do
@@ -25,9 +23,19 @@ class UserFlowTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should not login an user" do
+    post '/api/v1/users/login', params:{email: @user.email, password: 'nikhil1'}, as: :json
+    assert_equal 406, response.status
+  end
+
   test "update an user" do
     put "/api/v1/users/#{@user.id}", params: {admin_id: @admin.id, is_activated: !@user.is_activated}, as: :json
     assert_response :success
+  end
+
+  test "should not update an user" do
+    put "/api/v1/users/#{@user.id}", params: {admin_id: @user.id, is_activated: !@user.is_activated}, as: :json
+    assert_equal 401, response.status
   end
 
 end

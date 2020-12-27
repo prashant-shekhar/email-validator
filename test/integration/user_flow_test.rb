b5bfc5ded6e97c099789a2ccce89a7dfea10de96
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-12-27 17:18:09 +0530

    Integration test done

---
+++ test/integration/user_flow_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UserFlowTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin= users :admin
+    @user = users :valid
+  end
+
+  test "list the users" do
+    get "/api/v1/users", params: {user_id: @admin.id}, as: :json
+    assert_response :success
+  end
+
+  test "creating an user" do
+    post "/api/v1/users", params: {name: 'New User', email: 'nikhil.bhatt@elitmus.com' ,username: 'New_Username', password: 'sample_password', has_role: 'user', is_activated: true}, as: :json
+    assert_response :success
+  end
+
+  test "login an user" do
+    post "/api/v1/users/login", params: {email: @user.email, password: 'nikhil'}, as: :json
+    assert_response :success
+  end
+
+  test "update an user" do
+    put "/api/v1/users/#{@user.id}", params: {admin_id: @admin.id, is_activated: !@user.is_activated}, as: :json
+    assert_response :success
+  end
+
+end

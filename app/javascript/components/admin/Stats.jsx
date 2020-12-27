f6e283dddfabe85058c79f52c3211c23199c5250
maniSHarma7575 <hacker.sharma7575@gmail.com>
2020-12-27 11:50:00 +0530

    Bug-Fix admin dashboard card alignment

--- app/javascript/components/admin/Stats.jsx
+++ app/javascript/components/admin/Stats.jsx
@@ -8,24 +8,24 @@ export default class Stats extends Component {
     render() {
         return (
             <div className="row ">
-                <div className="col-4 ">
-                    <div className="card shadow p-3 mb-5 bg-white rounded">
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card shadow p-3 bg-white rounded h-100">
                         <div className="card-body">
                             <h5 className="card-title text-center">Total Users</h5>
                             <h2 className="card-text text-center">{this.props.totalUsers}</h2>
                         </div>
                     </div>
                 </div>
-                <div className="col-4">
-                    <div className="card shadow p-3 mb-5 bg-white rounded">
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card shadow p-3 bg-white rounded h-100">
                         <div className="card-body">
                             <h5 className="card-title text-center">User With CSV Permission</h5>
                             <h2 className="card-text text-center">{this.props.totalActivatedUsers}</h2>
                     </div>
                     </div>
                 </div>
-                <div className="col-4">
-                    <div className="card shadow p-3 mb-5 bg-white rounded">
+                <div className="col-sm-12 col-lg-4">
+                    <div className="card shadow p-3 bg-white rounded h-100">
                         <div className="card-body">
                             <h5 className="card-title text-center">User Without CSV Permission</h5>
                             <h2 className="card-text text-center">{this.props.totalUsers-this.props.totalActivatedUsers}</h2>

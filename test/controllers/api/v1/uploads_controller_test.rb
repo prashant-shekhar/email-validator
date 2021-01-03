fb7c94661b7a613bdd673e80d6fbe4b8ca53574d
maniSHarma7575 <hacker.sharma7575@gmail.com>
2021-01-03 10:06:20 +0530

    Uploads Controller Test fix

--- test/controllers/api/v1/uploads_controller_test.rb
+++ test/controllers/api/v1/uploads_controller_test.rb
@@ -1,14 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class Api::V1::UploadsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get api_v1_uploads_index_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get api_v1_uploads_create_url
-    assert_response :success
-  end
-
 end

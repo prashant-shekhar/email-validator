376420fd223c888977d83619e17389b2ef9ea69b
prashant-shekhar <toprashantshekhar@gmail.com>
2021-01-16 23:10:36 +0530

    Added the missing exception handling for the uploads_controller.

--- app/controllers/concerns/session_user_concern.rb
+++ app/controllers/concerns/session_user_concern.rb
@@ -2,7 +2,6 @@ module SessionUserConcern
   extend ActiveSupport::Concern
   def session_user
     decoded_hash = decoded_token
-    puts decoded_hash
     unless decoded_hash.empty?
       user_id = decoded_hash[0]["user_id"]
       @logged_in_user = User.find_by(id: user_id)
